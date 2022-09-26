2327a8cfb09d07fcc4aafd7170b3d341dee9b044
mauricioc12 <mauriciocontrerasr1202@gmail.com>
2022-09-25 23:53:40 -0500

    Updates

---
+++ precache-manifest.33b5134dfefcd56b99532a357725a40c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1acb829fba1d041264a3",
+    "url": "/ToDo-React-Project/static/css/main.60473ef7.chunk.css"
+  },
+  {
+    "revision": "1acb829fba1d041264a3",
+    "url": "/ToDo-React-Project/static/js/main.1acb829f.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/ToDo-React-Project/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "4663e27f58a7c977e52a",
+    "url": "/ToDo-React-Project/static/js/runtime~main.4663e27f.js"
+  },
+  {
+    "revision": "89c6e5ce793d1e91617267cc85acb83f",
+    "url": "/ToDo-React-Project/static/media/check.89c6e5ce.svg"
+  },
+  {
+    "revision": "fbc4bf146967ad3c197a9e999a0fa369",
+    "url": "/ToDo-React-Project/static/media/delete.fbc4bf14.svg"
+  },
+  {
+    "revision": "1b15653fa3b2e5304eb380bbc7121ac9",
+    "url": "/ToDo-React-Project/index.html"
+  }
+];
